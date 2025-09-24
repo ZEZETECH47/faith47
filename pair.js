4e5d37f1035bb8eadbe62c72dfe6ab0ae13f167f
ZEZETECH47 <humphreymbise75@gmail.com>
2025-09-24 16:15:10 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -76,14 +76,14 @@ try {
                     const megaUrl = await upload(fs.createReadStream(`${dirs}/creds.json`), `${generateRandomId()}.json`);
 
                     // Add "UMAR=" prefix to the session ID
-                    let stringSession = `${megaUrl.replace('https://mega.nz/file/', 'BLACK MD;;;')}`;
+                    let stringSession = `${megaUrl.replace('https://mega.nz/file/', 'FAITH MD;;;')}`;
 
                     // Send the session ID to the target number
                     await Um4r719.sendMessage(Um4r719.user.id, { text: stringSession });
 
                     // Send confirmation message
                     await Um4r719.sendMessage(Um4r719.user.id, { 
-                        text: 'BLACK MD has been successfully linked...Above is your session id, Copy and paste it on the SESSION string during deploy as it will be used for authentication.\n\nAny issue reach me here https://wa.me/message/4MX7ZQCAWRFBP1\n\nGoodluck 🎉.\n' 
+                        text: 'FAITH MD has been successfully linked...Above is your session id, Copy and paste it on the SESSION string during deploy as it will be used for authentication.\n\nAny issue reach me here https://wa.me/message/4MX7ZQCAWRFBP1\n\nGoodluck 🎉.\n' 
                     });
 
                     // Clean up session after use
